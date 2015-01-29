56366647096fa1c9ea2be18385e986b1209488c8
wqjung <wqjung@gmail.com>
2015-01-29 17:22:58 -0500

    Initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # statsd-agent
+
+Record system Metric such as CPU, Memory, Storage via to statsd, graphite
+
+sudo apt-get install supervisor
+git checkout https://github.com/etsy/statsd.git /usr/local/statsd
+
+sudo easy_install statsd
+sudo easy_install psutil
+
+copy ./conf/localConfig.js /usr/local/statsd
+
+copy ./conf/statsd.conf /etc/supervisior/conf.d
+copy ./conf/statsd-agent /etc/supervisor/conf.d
---
+++ statsd-agent.py
@@ -0,0 +1,62 @@
+import statsd
+import time
+import psutil
+import socket
+
+def disk(prefix):
+    disk_usage = psutil.disk_usage('/')
+    c.gauge(prefix + '.root.total', disk_usage.total)
+    c.gauge(prefix + '.root.used', disk_usage.used)
+    c.gauge(prefix + '.root.free', disk_usage.free)
+    c.gauge(prefix + '.root.percent', disk_usage.percent)
+            
+#         time.sleep(10)
+
+
+def cpu_times_percent(prefix):
+    value = psutil.cpu_percent(interval=1)
+    c.gauge(prefix + '.percent', value)
+
+    cpu_times_percent = psutil.cpu_times_percent(interval=1)
+    c.gauge(prefix + '.times_percent.user', cpu_times_percent.user)
+    c.gauge(prefix + '.times_percent.system', cpu_times_percent.system)
+    c.gauge(prefix + '.times_percent.idle', cpu_times_percent.idle)
+    if hasattr(cpu_times_percent, 'iowait'):
+        c.gauge(prefix + '.times_percent.iowait', cpu_times_percent.iowait)
+#         time.sleep(10)
+
+def memory(prefix):        
+    swap = psutil.swap_memory()
+    c.gauge(prefix + '.swap.total', swap.total)
+    c.gauge(prefix + '.swap.used', swap.used)
+    c.gauge(prefix + '.swap.free', swap.free)
+    c.gauge(prefix + '.swap.percent', swap.percent)
+
+    virtual = psutil.virtual_memory()
+    c.gauge(prefix + '.virtual.total', virtual.total)
+    c.gauge(prefix + '.virtual.available', virtual.available)
+    c.gauge(prefix + '.virtual.used', virtual.used)
+    c.gauge(prefix + '.virtual.free', virtual.free)
+    c.gauge(prefix + '.virtual.percent', virtual.percent)
+    if hasattr(virtual, 'active'):
+        c.gauge(prefix + '.virtual.active', virtual.active)
+    if hasattr(virtual, 'inactive'):
+        c.gauge(prefix + '.virtual.inactive', virtual.inactive)
+    if hasattr(virtual, 'buffers'):        
+        c.gauge(prefix + '.virtual.buffers', virtual.buffers)
+    if hasattr(virtual, 'cached'):
+        c.gauge(prefix + '.virtual.cached', virtual.cached)
+        
+#         time.sleep(10)
+        
+if __name__ == '__main__':
+    hostname = socket.gethostname().lower().replace('.', '_')
+    prefix = 'system.' + hostname
+    global c
+    c = statsd.StatsClient('localhost', 8125)
+    while True:
+        disk(prefix + '.disk')
+        cpu_times_percent(prefix + '.cpu')
+        memory(prefix  + '.memory')
+        time.sleep(10)
+
